93282af9d0cdc373944268c85622805ae7e853a7
odGnol <64988284+odGnol@users.noreply.github.com>
2021-05-16 18:34:18 +0200

    #13 pohyb myší v prostoru

--- src/control/Controller3D.java
+++ src/control/Controller3D.java
@@ -27,6 +27,8 @@ public class Controller3D {
     private Camera camera;
     private final double posun = 0.1;
     private boolean perspektiva = false;
+    private int aktualniX;
+    private int aktualniY;
 
     public Controller3D(Panel panel) {
         this.panel = panel;
@@ -77,54 +79,102 @@ public class Controller3D {
     }
 
     private void initListeners(Panel panel) {
-        panel.addKeyListener(
-                new KeyAdapter() {
-                    public void keyPressed(KeyEvent e) {
-                        if (e.getKeyCode() == KeyEvent.VK_C) {
-//                            imageBuffer.setElement(400, 300, Color.YELLOW.getRGB());
-//                            panel.repaint();
-                            initBuffers();
-                        } else if (e.getKeyCode() == KeyEvent.VK_E) {
-                            partBuffer = new ArrayList<>();
-                            indexBuffer = new ArrayList<>();
-                            vertexBuffer = new ArrayList<>();
+        panel.addKeyListener(new KeyAdapter() {
+                                 public void keyPressed(KeyEvent e) {
+                                     if (e.getKeyCode() == KeyEvent.VK_C) {
+                                         initBuffers();
+                                     } else if (e.getKeyCode() == KeyEvent.VK_E) {
+                                         partBuffer = new ArrayList<>();
+                                         indexBuffer = new ArrayList<>();
+                                         vertexBuffer = new ArrayList<>();
 
-                        } else if (e.getKeyCode() == KeyEvent.VK_W) {
-                            camera = camera.up(posun);
+                                     } else if (e.getKeyCode() == KeyEvent.VK_W) {
+                                         camera = camera.up(posun);
 
-                        } else if (e.getKeyCode() == KeyEvent.VK_S) {
-                            camera = camera.down(posun);
+                                     } else if (e.getKeyCode() == KeyEvent.VK_S) {
+                                         camera = camera.down(posun);
 
-                        } else if (e.getKeyCode() == KeyEvent.VK_A) {
-                            camera = camera.left(posun);
+                                     } else if (e.getKeyCode() == KeyEvent.VK_A) {
+                                         camera = camera.left(posun);
 
-                        } else if (e.getKeyCode() == KeyEvent.VK_D) {
-                            camera = camera.right(posun);
+                                     } else if (e.getKeyCode() == KeyEvent.VK_D) {
+                                         camera = camera.right(posun);
 
-                        } else if (e.getKeyCode() == KeyEvent.VK_P) {
-                            if (perspektiva) {
-                                perspektiva = false;
-                                projection = new Mat4OrthoRH(
-                                        10.0,
-                                        10.0 * (imageBuffer.getHeight() / (float) imageBuffer.getWidth()),
-                                        0.5,
-                                        50
-                                );
-                            } else {
-                                perspektiva = true;
-                                projection = new Mat4PerspRH(
-                                        Math.PI / 3,
-                                        imageBuffer.getHeight() / (float) imageBuffer.getWidth(),
-                                        0.5,
-                                        50
-                                );
-                            }
-                        }
+                                     } else if (e.getKeyCode() == KeyEvent.VK_P) {
+                                         if (perspektiva) {
+                                             perspektiva = false;
+                                             projection = new Mat4OrthoRH(
+                                                     10.0,
+                                                     10.0 * (imageBuffer.getHeight() / (float) imageBuffer.getWidth()),
+                                                     0.5,
+                                                     50
+                                             );
+                                         } else {
+                                             perspektiva = true;
+                                             projection = new Mat4PerspRH(
+                                                     Math.PI / 3,
+                                                     imageBuffer.getHeight() / (float) imageBuffer.getWidth(),
+                                                     0.5,
+                                                     50
+                                             );
+                                         }
+                                     }
 
-                        display();
-                    }
-                }
+                                     display();
+                                 }
+                             }
         );
+
+        panel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                aktualniX = e.getX();
+                aktualniY = e.getY();
+            }
+        });
+
+        panel.addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseDragged(MouseEvent e) {
+                if (aktualniX < e.getX()) {
+                    camera = camera.addAzimuth(-(e.getX() - aktualniX) / 1000.0);
+                    aktualniX = e.getX();
+                } else if (e.getX() < aktualniX) {
+                    camera = camera.addAzimuth((aktualniX - e.getX()) / 1000.0);
+                    aktualniX = e.getX();
+                }
+                if (aktualniY < e.getY()) {
+                    camera = camera.addZenith(-(e.getY() - aktualniY) / 1000.0);
+                    aktualniY = e.getY();
+                } else if (e.getY() < aktualniY) {
+                    camera = camera.addZenith((aktualniY - e.getY()) / 1000.0);
+                    aktualniY = e.getY();
+                }
+
+                display();
+            }
+        });
+
+        panel.addMouseWheelListener(new MouseAdapter() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if (e.getWheelRotation() == -1) {
+                    Mat4 mat = new Mat4Scale(1.1, 1.1, 1.1);
+                    model = model.mul(mat);
+
+                } else {
+                    Mat4 mat = new Mat4Scale(0.9, 0.9, 0.9);
+                    model = model.mul(mat);
+                }
+                display();
+            }
+        });
+
     }
 
     private void initBuffers() {
@@ -142,8 +192,6 @@ public class Controller3D {
         indexBuffer.add(3);
 
         partBuffer.add(new Part(TopologyType.LINE, 0, 6));
-
-        System.out.println(TopologyType.POINT);
     }
 
 }

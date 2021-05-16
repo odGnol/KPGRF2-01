7cf9cf718ddfcc9bc4af99dcbb51202a94653240
odGnol <64988284+odGnol@users.noreply.github.com>
2021-05-16 20:00:26 +0200

    #2 Osy obarveny jednotnými barvami

--- src/control/Controller3D.java
+++ src/control/Controller3D.java
@@ -119,7 +119,6 @@ public class Controller3D {
                                              );
                                          }
                                      }
-
                                      display();
                                  }
                              }
@@ -178,18 +177,22 @@ public class Controller3D {
     }
 
     private void initBuffers() {
+
+        //vrcholy pro osy XYZ(gizmo)
         vertexBuffer.add(new Vertex(new Point3D(), new Col(255, 0, 0)));
-        vertexBuffer.add(new Vertex(new Point3D(1, 0, 0), new Col(255, 0, 0)));  //1
-        vertexBuffer.add(new Vertex(new Point3D(0, 1, 0), new Col(0, 255, 0))); //2
-        vertexBuffer.add(new Vertex(new Point3D(0, 0, 1), new Col(0, 0, 255))); //3
+        vertexBuffer.add(new Vertex(new Point3D(), new Col(0, 255, 0)));
+        vertexBuffer.add(new Vertex(new Point3D(), new Col(0, 0, 255)));
+        vertexBuffer.add(new Vertex(new Point3D(1, 0, 0), new Col(255, 0, 0)));
+        vertexBuffer.add(new Vertex(new Point3D(0, 1, 0), new Col(0, 255, 0)));
+        vertexBuffer.add(new Vertex(new Point3D(0, 0, 1), new Col(0, 0, 255)));
 
         //osy XYZ
         indexBuffer.add(0);
-        indexBuffer.add(1);
-        indexBuffer.add(0);
-        indexBuffer.add(2);
-        indexBuffer.add(0);
         indexBuffer.add(3);
+        indexBuffer.add(1);
+        indexBuffer.add(4);
+        indexBuffer.add(2);
+        indexBuffer.add(5);
 
         partBuffer.add(new Part(TopologyType.LINE, 0, 6));
     }
